6452b58d30bd46c977ad3b63cd050180294f4eee
graf <oldestgraf@gmail.com>
2025-01-09 11:42:08 +0100

    Done

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,9 +1,17 @@
-export const fetchPhotosByQuery = searchedQuery => {
-    return fetch(`https://pixabay.com/api/?${searchedQuery}`).then(response => {
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
+import axios from "axios";
 
-        return response.json();
-    })
+axios.defaults.baseURL = 'https://pixabay.com';
+
+export const fetchPhotosByQuery = (searchedQuery, page) => {
+    const requestParams = {
+        q: searchedQuery,
+        page: page,
+        key: "47600623-616adcc60326fea30dcc03763",
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true,
+        per_page: 15,
+    };
+
+    return axios.get('/api', { params: requestParams });
 }
